972243b8ca9684bd4414209b0fe2cc3d1d21d4ad
celtichero2026 <celtichero2026@gmail.com>
2026-04-13 17:31:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -629,11 +629,11 @@ async def check_due_boss_pings():
             continue
 
         # create 1-minute-before alert and leave it through the window
-        if 0 <= seconds_until_open <= 120 and boss_key not in active_alert_messages:
+       if 120 < seconds_until_open <= 180 and boss_key not in active_alert_messages:
             role_id = get_ping_role_id(boss_key)
             boss_name = BOSSES[boss_key]["display"]
 
-            message_text = f"{boss_name} is due in 1 minute."
+            message_text = f"{boss_name} is due in 3 minute."
             if role_id:
                 message_text = f"<@&{role_id}> 🚨 {boss_name} is due. 🚨"
 
